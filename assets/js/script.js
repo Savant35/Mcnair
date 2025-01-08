3970ac3d0a4a08f47530822d51cbd9ea275cb57f
Ali Arthur <121364807+Savant35@users.noreply.github.com>
2025-01-08 15:16:52 -0500

    finished navigation menu adding function for showing and hiding the menu

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu");
+
+if (navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
+
+if (navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
+
+const nav = document.querySelectorAll('.nav__link');
+const action = () => {
+  navMenu.classList.remove('show-menu');
+}
+
+nav.forEach(n => n.addEventListener('click', action));
+
+
